47d6413ea58bcd76f231b8efff14758c7f173a36
ltndat <ltndat@outlook.com>
2022-03-15 11:20:24 +0700

    add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <Helmet>
         <title>My Title</title>
         <meta name="description" content="Helmet application" />
+        <link rel="canonical" href="https://kind-mayer-cf61b6.netlify.app/" />
         <meta property="og:title" content="Og Title" />
         <meta property="og:description" content="Og description" />
       </Helmet>
