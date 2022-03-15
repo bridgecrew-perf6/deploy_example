7d98d2969bc6bfea9394d07e692df2e3dca1deea
ltndat <ltndat@outlook.com>
2022-03-15 14:14:49 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
+import { Helmet } from 'react-helmet';
 import { SEO } from './SEO';
 
 function App() {
-  const title = 'My Title'
-  const description = 'About my testing site'
+  const title = 'My Title';
+  const description = 'About my testing site';
 
   return (
     <div className="App">
-      <SEO
-        title={title}
-        description={description}
-        meta={[
-          {
-            property: 'og:type',
-            content: 'website',
-          },
-          {
-            property: "og:url",
-            content: window.location.href,
-          },
-          {
-            property: "og:title",
-            content: title,
-          },
-          {
-            property: "og:description",
-            content: description,
-          },
-          {
-            property: "og:image",
-            content: '/logo192.png',
-          },
-        ]}
-      />
+      <Helmet>
+        <title>Look good</title>
+        <meta name="description" content="About my testing site" />
+
+        <meta itemprop="name" content="Look good" />
+        <meta itemprop="description" content="About my testing site" />
+        <meta
+          itemprop="image"
+          content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShtbUdknvmGqaAcEFvneCbBcSKAYDTatzbefpZ_SlRu2Oz3KLvvEopGNOHQkS-yx3T3Jk&usqp=CAU"
+        />
+
+        <meta
+          property="og:url"
+          content="https://kind-mayer-cf61b6.netlify.app"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Look good" />
+        <meta property="og:description" content="About my testing site" />
+        <meta
+          property="og:image"
+          content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShtbUdknvmGqaAcEFvneCbBcSKAYDTatzbefpZ_SlRu2Oz3KLvvEopGNOHQkS-yx3T3Jk&usqp=CAU"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Look good" />
+        <meta name="twitter:description" content="About my testing site" />
+        <meta
+          name="twitter:image"
+          content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShtbUdknvmGqaAcEFvneCbBcSKAYDTatzbefpZ_SlRu2Oz3KLvvEopGNOHQkS-yx3T3Jk&usqp=CAU"
+        />
+      </Helmet>
       App
     </div>
   );
